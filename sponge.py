cfc87206c54409b1b6bf41a2bee6a7cfecfa3a78
Eren Uğur <114035425+erenugur@users.noreply.github.com>
2023-05-22 10:27:24 -0700

    Update sponge.py

--- sponge.py
+++ sponge.py
@@ -1,6 +1,5 @@
 # Setup
 class Character:
-
     def __init__(self, name, laugh_type):
         self.name = name
         self.laugh_type = laugh_type
@@ -8,7 +7,7 @@ class Character:
     def laugh(self):
         print(f"{self.name}: {self.laugh_type}")
 
-
+        
 sponge = Character("Spongebob", "Hahahaha")
 paper_sponge = Character("Doodlebob", "NEHOY MENOY HOY MENYOY")
 crab = Character("Mr. Krabs", "Ar ar ar ar")
